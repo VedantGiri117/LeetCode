f813135348e78d901055f54f0747335bfded28d3
VedantGiri117 <vedantgiri117@gmail.com>
2026-02-21 17:00:01 +0530

    Add solution for Assign Cookies problem
    Implement solution for the Assign Cookies problem using sorting and two pointers.

---
+++ 455-assign-cookies/solution.java
@@ -0,0 +1,37 @@
+/*
+Problem: Assign Cookies
+Link: https://leetcode.com/problems/assign-cookies/
+
+Approach:
+- Sort both arrays
+- Use two pointers
+- Assign smallest valid cookie
+
+Time Complexity: O(n log n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0,j = 0;
+        //if(n < m) return g.length;
+
+        while(i < n && j < m ){
+            if(s[j] >= g[i]){
+                i++;
+            }
+            j++;
+        }
+        return  i ;
+        
+    }
+}     
+
+
+
+Add: Assign Cookies solution 
